6687ac1f14cd1c9bba4e81d320bbcd8869cf2210
OleBasten <ole.basten@edu-kw1c.nl>
2026-03-16 10:01:21 +0100

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+// Wisselt tussen light en dark mode
+function toggleMode() { 
+  const body = document.body; // pakt het body-element
+  const button = document.querySelector('.Dark-Mode-Knop'); // pakt de knop
+
+  body.classList.toggle('dark-mode'); // zet dark-mode aan of uit
+
+  // Verander de tekst op de knop
+  if (body.classList.contains('dark-mode')) {
+    button.textContent = '☀️ Light mode';
+  } else {
+    button.textContent = '🌙 Dark mode';
+  }
+}
+
+// Zoek de knop op met het id 'mijnKnop'
+const knop = document.getElementById('Copyright-knop');
+
+// Voeg een klik-event toe
+knop.addEventListener('click', function() {
+  alert('Ole Basten, Alle Rechten Voorbehouden. Ole.Basten@edu-kw1c.nl');
+});
+
